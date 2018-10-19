71a392ef64c7e43e8a6f70379d103e3f6130da0a
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-19 16:41:03 +0200

    Added food controller

---
+++ Cookbook.Web.Server/Controllers/FoodController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cookbook.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookbook.Web.Server.Controllers {
+    [Route("api/food")]
+    public class FoodController {
+
+        [HttpGet]
+        public async Task<IEnumerable<FoodDto>> GetAllFood() {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<FoodDto> Get(int id) {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task Create(int id, [FromBody] FoodCreate food) {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task Update(int id, [FromBody] FoodUpdate food) {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public async Task Delete(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}
