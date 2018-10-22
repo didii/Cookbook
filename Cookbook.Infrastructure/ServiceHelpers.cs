b85a7993e3ac04fbd24ce883d00f1333bb925301
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-22 17:30:51 +0200

    DI helper

---
+++ Cookbook.Infrastructure/ServiceHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cookbook.Infrastructure {
+    public static class ServiceHelpers {
+        public static void AddRule(this IServiceCollection services, Assembly assembly, string suffix) {
+            var interfaces = assembly.ExportedTypes.Where(t => t.IsInterface && t.Name.EndsWith(suffix));
+            foreach (var @interface in interfaces) {
+                var @class = assembly.DefinedTypes.FirstOrDefault(t => t.IsClass && t.Name == @interface.Name.Substring(1));
+                if (@class == null)
+                    continue;
+                services.AddScoped(@interface, @class);
+            }
+        }
+    }
+}
