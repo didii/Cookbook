7c39959d21160446f4396a8935ab3053d4f1f07e
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-22 17:30:22 +0200

    Empty mapper profile

---
+++ Cookbook.Business/Mapper/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Cookbook.Business.Mapper {
+    class MapperProfile : Profile {
+        public MapperProfile() {
+            
+        }
+    }
+}
