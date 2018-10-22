285b1f9e8c4cf487481100f3a03fc52143b9fc56
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-22 17:31:17 +0200

    Added swagger + exception filter

---
+++ Cookbook.Web.Server/Filters/ExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+
+namespace Cookbook.Web.Server.Filters {
+    public class ExceptionFilter : ExceptionFilterAttribute {
+        private readonly IHostingEnvironment _env;
+        private readonly IModelMetadataProvider _modelMetadataProvider;
+
+        public ExceptionFilter(IHostingEnvironment env, IModelMetadataProvider modelMetadataProvider) {
+            _env = env;
+            _modelMetadataProvider = modelMetadataProvider;
+        }
+
+        public override void OnException(ExceptionContext context) {
+            context.Result = new JsonResult(context.Exception, new JsonSerializerSettings() {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+        }
+    }
+}
--- Cookbook.Web.Server/Startup.cs
+++ Cookbook.Web.Server/Startup.cs
@@ -1,17 +1,34 @@
 using System.Linq;
 using System.Net.Mime;
+using Cookbook.Business;
+using Cookbook.Web.Server.Filters;
 using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Cookbook.Web.Server {
     public class Startup {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfiguration _config;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public Startup(IHostingEnvironment env, IConfiguration config, ILoggerFactory loggerFactory) {
+            _env = env;
+            _config = config;
+            _loggerFactory = loggerFactory;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+            services.AddMvc(x => x.Filters.Add<ExceptionFilter>());
+
+            services.AddSwaggerGen(x => { x.SwaggerDoc("v1", new Info() { Title = "Cookbook", Description = "The cookbook recipe API" }); });
 
             services.AddResponseCompression(options => {
                 options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] {
@@ -19,17 +36,24 @@ namespace Cookbook.Web.Server {
                     WasmMediaTypeNames.Application.Wasm,
                 });
             });
+
+            services.AddBusinessServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             app.UseResponseCompression();
 
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment()) {
+            //    app.UseDeveloperExceptionPage();
+            //}
 
             app.UseMvc(routes => { routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}"); });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x => {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "Cookbook");
+            });
 
             app.UseBlazor<Client.Startup>();
         }
