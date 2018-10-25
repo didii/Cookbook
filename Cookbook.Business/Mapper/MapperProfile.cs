6e8b69d64d44d02944c5ff6ce15f9ea12e3b9d22
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-25 21:46:28 +0200

    Added mappings for food

--- Cookbook.Business/Mapper/MapperProfile.cs
+++ Cookbook.Business/Mapper/MapperProfile.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using AutoMapper;
+using Cookbook.Domain;
+using Cookbook.Dtos;
 
 namespace Cookbook.Business.Mapper {
     class MapperProfile : Profile {
         public MapperProfile() {
-            
+            CreateMap<Food, FoodDto>();
+            CreateMap<FoodCreate, Food>();
+            CreateMap<FoodUpdate, Food>();
         }
     }
 }
