f47d97a3b6b69f04a5422627ebbc8495b1c147e7
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-30 00:25:59 +0100

    ToJson extension

---
+++ Cookbook.Web.Client/Helpers/JsonConvertExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.JSInterop;
+
+namespace Cookbook.Web.Client.Helpers {
+    public static class JsonConvertExtensions {
+        public static string ToJson(this object obj) {
+            return Json.Serialize(obj);
+        }
+    }
+}
