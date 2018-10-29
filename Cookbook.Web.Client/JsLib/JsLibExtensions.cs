d3feefaab9b4f8cee42a4ae8571b0b01a92ee5f9
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-30 00:26:16 +0100

    JsLib extensions

---
+++ Cookbook.Web.Client/JsLib/JsLibExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.JSInterop;
+
+namespace Cookbook.Web.Client.JsLib {
+    public static class JsLibExtensions {
+        public static Task Focus(this ElementRef elementRef) {
+            return JSRuntime.Current.InvokeAsync<object>("jsLib.focusElement", elementRef);
+        }
+
+        public static Task Hide(this ElementRef elementRef) {
+            return JSRuntime.Current.InvokeAsync<object>("jsLib.hideElement", elementRef);
+        }
+
+        public static Task Show(this ElementRef elementRef) {
+            return JSRuntime.Current.InvokeAsync<object>("jsLib.showElement", elementRef);
+        }
+    }
+}
---
+++ Cookbook.Web.Client/wwwroot/js/jsLib.js
@@ -0,0 +1,11 @@
+﻿window.jsLib = {
+    focusElement: function(element) {
+        element.focus();
+    },
+    hideElement: function(element) {
+        element.setAttribute("hidden", true);
+    },
+    showElement: function(element) {
+        element.removeAttribute("hidden");
+    }
+}
