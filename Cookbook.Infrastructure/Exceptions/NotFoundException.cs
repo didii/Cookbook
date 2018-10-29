b815ccebd7d60715496b923833a5e9c0dfa8e074
Dieter Van Broeck <VLM\OrdinaDVB>
2018-10-29 13:18:03 +0100

    Created ExceptionHandler middleware

---
+++ Cookbook.Infrastructure/Exceptions/NotFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cookbook.Infrastructure.Exceptions {
+    public class NotFoundException : Exception {
+        private const string _message1 = "Could not find entity {0}";
+        private const string _message2 = _message1 + " with ID {1}";
+
+        private static string CreateMessage(Type type) {
+            return string.Format(_message1, type.Name);
+        }
+        private static string CreateMessage(Type type, object id) {
+            return string.Format(_message2, type.Name, id);
+        }
+
+        public NotFoundException(Type type, object id) : base(CreateMessage(type)) {
+            EntityType = type;
+            EntityId = id;
+        }
+
+        public NotFoundException(Type type, object id, Exception innerException) : base(CreateMessage(type), innerException) {
+            EntityType = type;
+            EntityId = id;
+        }
+
+        public Type EntityType { get; }
+
+        public object EntityId { get; }
+    }
+}
---
+++ Cookbook.Web.Server/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Cookbook.Infrastructure.Exceptions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Cookbook.Web.Server.Middleware {
+    public class ExceptionHandlerMiddleware {
+        private readonly RequestDelegate _next;
+        private readonly IHostingEnvironment _env;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, IHostingEnvironment env) {
+            _next = next;
+            _env = env;
+        }
+
+        public async Task Invoke(HttpContext context) {
+            try {
+                await _next.Invoke(context);
+            }
+            catch (Exception e) when (_env.IsDevelopment()) {
+                var body = CreateExceptionBody(e, out var code);
+                SetResponse(context, code, body);
+            }
+        }
+
+        private void SetResponse(HttpContext context, HttpStatusCode status, JObject body) {
+            if (!context.Response.HasStarted) {
+                context.Response.StatusCode = (int)status;
+                context.Response.WriteAsync(body.ToString());
+            }
+        }
+
+        private JObject CreateExceptionBody(Exception e, out HttpStatusCode statusCode) {
+            var result = new JObject {
+                ["ExceptionType"] = e.GetType().FullName,
+                [nameof(e.Message)] = e.Message,
+                [nameof(e.StackTrace)] = e.StackTrace,
+                [nameof(e.Data)] = JToken.FromObject(e.Data),
+            };
+            if (e.InnerException != null)
+                result[nameof(e.InnerException)] = CreateExceptionBody(e.InnerException, out _);
+            switch (e) {
+                case NotFoundException notFound:
+                    statusCode = HttpStatusCode.NotFound;
+                    result[nameof(notFound.EntityType)] = notFound.EntityType.FullName;
+                    result[nameof(notFound.EntityId)] = JToken.FromObject(notFound.EntityId);
+                    break;
+                default:
+                    statusCode = HttpStatusCode.InternalServerError;
+                    break;
+            }
+            return result;
+        }
+    }
+}
