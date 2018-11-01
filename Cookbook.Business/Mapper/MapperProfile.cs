db5ebd04b8b4c0897c849b2df967779c09316f49
Dieter Van Broeck <VLM\OrdinaDVB>
2018-11-01 23:31:41 +0100

    Mapping

--- Cookbook.Business/Mapper/MapperProfile.cs
+++ Cookbook.Business/Mapper/MapperProfile.cs
@@ -8,9 +8,16 @@ using Cookbook.Dtos;
 namespace Cookbook.Business.Mapper {
     class MapperProfile : Profile {
         public MapperProfile() {
+            // Food
             CreateMap<Food, FoodDto>();
             CreateMap<FoodCreate, Food>();
             CreateMap<FoodUpdate, Food>();
+
+            // Recipe
+            CreateMap<Recipe, RecipeDto>();
+
+            // Tags
+            CreateMap<AppliedTag, TagDto>().Substitute(x => x.Tag);
         }
     }
 }
