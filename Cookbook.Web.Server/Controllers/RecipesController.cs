cfc2356624f953fef0e83c1f4c8c2d049ebe0149
Dieter Van Broeck <VLM\OrdinaDVB>
2018-11-01 23:32:01 +0100

    Added recipes and tag controllers

---
+++ Cookbook.Web.Server/Controllers/RecipesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Cookbook.Business.RecipeServices;
+using Cookbook.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookbook.Web.Server.Controllers {
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipesController : Controller {
+        private readonly IRecipeService _recipeService;
+
+        public RecipesController(IRecipeService recipeService) {
+            _recipeService = recipeService;
+        }
+
+        [HttpPost]
+        public async Task<long> Create() {
+            return await _recipeService.CreateEmptyAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<RecipeDto> Get(int id) {
+            return await _recipeService.GetAsync(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] RecipeUpdate recipe) {
+            await _recipeService.UpdateAsync(id, recipe);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id) {
+            await _recipeService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Cookbook.Web.Server/Controllers/TagController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cookbook.Business.RecipeServices;
+using Cookbook.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookbook.Web.Server.Controllers {
+    [ApiController]
+    [Route("api/tags")]
+    public class TagController : Controller {
+        private readonly ITagService _tagService;
+
+        public TagController(ITagService tagService) {
+            _tagService = tagService;
+        }
+
+        [HttpPost]
+        public async Task<long> Create([FromBody] TagEdit tag) {
+            return await _tagService.CreateAsync(tag);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, [FromBody] TagEdit tag) {
+            await _tagService.UpdateAsync(id, tag);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id) {
+            await _tagService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
