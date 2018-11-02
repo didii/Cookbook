0fd0bd85eb08fced818fb9af48a26503ba46b728
Dieter Van Broeck <VLM\OrdinaDVB>
2018-11-02 10:40:22 +0100

    Register all services

--- Cookbook.Web.Client/Startup.cs
+++ Cookbook.Web.Client/Startup.cs
@@ -6,6 +6,8 @@ namespace Cookbook.Web.Client {
     public class Startup {
         public void ConfigureServices(IServiceCollection services) {
             services.AddSingleton<IDetailsService, DetailsService>();
+
+            services.AddServices();
         }
 
         public void Configure(IBlazorApplicationBuilder app) {
