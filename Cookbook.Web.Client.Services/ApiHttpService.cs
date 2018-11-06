a22e533482b82de55098b4f2a940b557dc682b3c
Dieter Van Broeck <VLM\OrdinaDVB>
2018-11-06 21:05:56 +0100

    Added IApiHttpService

---
+++ Cookbook.Web.Client.Services/ApiHttpService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Cookbook.Web.Client.Services.Exceptions;
+using Microsoft.JSInterop;
+
+namespace Cookbook.Web.Client.Services {
+    class ApiHttpService : IApiHttpService {
+        private readonly HttpClient _http;
+
+        public ApiHttpService(HttpClient http) {
+            _http = http;
+        }
+
+        /// <inheritdoc />
+        public async Task<HttpResponseMessage> GetAsync(string uri) {
+            return await SendAsync(HttpMethod.Get, uri);
+        }
+
+        /// <inheritdoc />
+        public async Task<T> GetAsync<T>(string uri) {
+            return await SendAsync<T>(HttpMethod.Get, uri);
+        }
+
+        /// <inheritdoc />
+        public async Task<HttpResponseMessage> PostAsync(string uri, object body = null) {
+            return await SendAsync(HttpMethod.Post, uri, body);
+        }
+
+        /// <inheritdoc />
+        public async Task<T> PostAsync<T>(string uri, object body = null) {
+            return await SendAsync<T>(HttpMethod.Post, uri, body);
+        }
+
+        /// <inheritdoc />
+        public async Task<HttpResponseMessage> PutAsync(string uri, object body = null) {
+            return await SendAsync(HttpMethod.Put, uri, body);
+        }
+
+        /// <inheritdoc />
+        public async Task<T> PutAsync<T>(string uri, object body = null) {
+            return await SendAsync<T>(HttpMethod.Put, uri, body);
+        }
+
+        /// <inheritdoc />
+        public async Task<HttpResponseMessage> DeleteAsync(string uri) {
+            return await SendAsync(HttpMethod.Delete, uri);
+        }
+
+        /// <inheritdoc />
+        public async Task<T> DeleteAsync<T>(string uri) {
+            return await SendAsync<T>(HttpMethod.Delete, uri);
+        }
+
+        private async Task<HttpResponseMessage> SendAsync(HttpMethod method, string uri, object body = null) {
+            //Console.WriteLine($"Http {method} - {uri}");
+            var request = new HttpRequestMessage(method, uri);
+            if (body != null) {
+                var jsonBody = Json.Serialize(body);
+                request.Content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+            }
+            return await _http.SendAsync(request);
+        }
+
+        private async Task<T> SendAsync<T>(HttpMethod method, string uri, object body = null) {
+            var response = await SendAsync(method, uri, body);
+            if (response.IsSuccessStatusCode) {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return Json.Deserialize<T>(responseBody);
+            }
+            throw new HttpResponseException(response);
+        }
+    }
+}
---
+++ Cookbook.Web.Client.Services/IApiHttpService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cookbook.Web.Client.Services {
+    public interface IApiHttpService {
+        Task<HttpResponseMessage> GetAsync(string uri);
+        Task<T> GetAsync<T>(string uri);
+        Task<HttpResponseMessage> PostAsync(string uri, object body = null);
+        Task<T> PostAsync<T>(string uri, object body = null);
+        Task<HttpResponseMessage> PutAsync(string uri, object body = null);
+        Task<T> PutAsync<T>(string uri, object body = null);
+        Task<HttpResponseMessage> DeleteAsync(string uri);
+        Task<T> DeleteAsync<T>(string uri);
+    }
+}
