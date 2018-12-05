3730799b15c0f1fe86264676103215b2f5ef1768
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-05 20:56:46 +0100

    EditableString component fully works

---
+++ Cookbook.Web.Client/Components/EditableComponent/EditableModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook.Web.Client.Components.EditableComponent {
+    public class EditableModel<T> {
+        public T OriginalValue { get; set; }
+        public T CurrentValue { get; set; }
+    }
+}
