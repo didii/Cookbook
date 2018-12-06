d7fe0408d73c3fd04a5596cfe8ecab6664e8801e
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-06 23:03:21 +0100

    Removed console.log

--- Cookbook.Web.Client/wwwroot/js/jsLib.js
+++ Cookbook.Web.Client/wwwroot/js/jsLib.js
@@ -1,14 +1,11 @@
 ﻿window.jsLib = {
     focusElement: function(element) {
-        console.log('focusElement(' + element.id + ')');
         element.focus();
     },
     hideElement: function(element) {
-        console.log('hideElement(' + element.id + ')');
         element.setAttribute("hidden", true);
     },
     showElement: function(element) {
-        console.log('showElement(' + element.id + ')');
         element.removeAttribute("hidden");
     }
 };
