3e85cc5b5f84f7091b9a11b477c005b0a68ab29f
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-13 00:26:30 +0100

    Some other necessary comments

--- Cookbook.Web.Client.Services/RecipeExtensions.cs
+++ Cookbook.Web.Client.Services/RecipeExtensions.cs
@@ -6,7 +6,13 @@ using Microsoft.JSInterop;
 
 namespace Cookbook.Web.Client.Services {
     public static class RecipeExtensions {
+        /// <summary>
+        /// Converts the recipe to its update variant
+        /// </summary>
+        /// <param name="recipe"></param>
+        /// <returns></returns>
         public static RecipeUpdate ToUpdate(this RecipeDto recipe) {
+            //TODO: no, just no
             var json = Json.Serialize(recipe);
             return Json.Deserialize<RecipeUpdate>(json);
         }
