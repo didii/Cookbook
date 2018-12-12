9b22bd232adf52c32fd3bdc3aed083ad15e0cce0
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-13 00:27:05 +0100

    Testing stuff
    Result: I was dumb

---
+++ Cookbook.Web.Client/Components/TestComponent/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook.Web.Client.Components.TestComponent {
+    public class Model {
+        public int Id { get;set; }
+        public string Name { get; set; }
+        public SubModel SubModel { get; set; }
+    }
+
+    public class SubModel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public SubSubModel SubSubModel { get;set; }
+    }
+
+    public class SubSubModel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
