b63f2d11fb9e7c9051ceb3cdfdba5027a4915a7b
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-13 00:00:30 +0100

    More seed data

--- Cookbook.Sql/CreateRecipe.sql
+++ Cookbook.Sql/CreateRecipe.sql
@@ -38,3 +38,47 @@ INSERT INTO [dbo].[AppliedTag]
            (1, 2, GETDATE(), GETDATE()),
            (1, 3, GETDATE(), GETDATE())
 GO
+
+SET IDENTITY_INSERT dbo.QuantityType ON
+INSERT INTO [dbo].[QuantityType]
+           ([Id], [Name], [CreatedOn], [UpdatedOn])
+     VALUES
+           (1, 'Massa', GetDate(), GetDate()),
+		   (2, 'Volume', GetDate(), GetDate()),
+		   (3, 'Eenheid', GetDate(), GetDate())
+GO
+SET IDENTITY_INSERT dbo.QuantityType OFF
+
+SET IDENTITY_INSERT dbo.Quantity ON
+INSERT INTO [dbo].[Quantity]
+           ([Id], [Name], [ShortName], [Multiplier], [QuantityTypeId], [IsBaseQuantity], [CreatedOn], [UpdatedOn])
+     VALUES
+           (1, 'een snufje', NULL, 1, 3, 0, GetDate(), GetDate()),
+           (2, NULL, NULL, 1, 3, 1, GetDate(), GetDate()),
+           (3, 'gram', 'g', 1, 1, 1, GetDate(), GetDate())
+GO
+SET IDENTITY_INSERT dbo.Quantity OFF
+
+SET IDENTITY_INSERT dbo.Food ON
+INSERT INTO [dbo].[Food]
+           ([Id], [Name], [NamePlural], [Description], [CreatedOn], [UpdatedOn])
+     VALUES
+           (1, 'Zout', 'Zout', NULL, GetDate(), GetDate()),
+		   (2, 'Peper', 'Peper', 'Te veel peper kan het gerecht pikant maken', GetDate(), GetDate()),
+		   (3, 'Spirelli', 'Spirelli', 'De normale lange slierten spaghetti', GetDate(), GetDate()),
+		   (4, 'Ei', 'Eieren', 'Nevenproduct van een kip', GetDate(), GetDate()),
+		   (5, 'Parmazaanse kaas', 'Parmazaanse kaas', 'Echte Italiaanse kaas', GetDate(), GetDate())
+GO
+SET IDENTITY_INSERT dbo.Food OFF
+
+SET IDENTITY_INSERT dbo.Ingredient ON
+INSERT INTO [dbo].[Ingredient]
+           ([Id], [FoodId], [QuantityId], [QuantityValue], [CreatedOn], [UpdatedOn], [RecipeId])
+     VALUES
+           (1, 1, 1, NULL, GetDate(), GetDate(), 1),
+           (2, 2, 1, NULL, GetDate(), GetDate(), 1),
+           (3, 3, 3, 500, GetDate(), GetDate(), 1),
+           (4, 4, 2, 2, GetDate(), GetDate(), 1),
+           (5, 5, 3, 200, GetDate(), GetDate(), 1)
+GO
+SET IDENTITY_INSERT dbo.Ingredient OFF
