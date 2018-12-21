1774c673beaaa01ce0421adbcda090e869ee7315
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-21 08:43:02 +0100

    Made EditableDetailTable have less parameters

---
+++ Cookbook.Web.Client/Components/DetailTable/ItemChangedArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook.Web.Client.Components.DetailTable {
+    public struct ItemChangedArgs<T> {
+        public T Item { get; set; }
+        public ItemChangedType Type { get; set; }
+        public int? Index { get; set; }
+    }
+
+    public enum ItemChangedType {
+        Added,
+        Changed,
+        Deleted
+    }
+}
