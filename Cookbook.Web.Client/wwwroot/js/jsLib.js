5189f891ed108a4b27ba9b87fb0c28565df2e655
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-24 08:16:20 +0100

    Trying out some autoselect js libraries

--- Cookbook.Web.Client/wwwroot/js/jsLib.js
+++ Cookbook.Web.Client/wwwroot/js/jsLib.js
@@ -7,5 +7,18 @@
     },
     showElement: function(element) {
         element.removeAttribute("hidden");
+    },
+    createAutocomplete: function(element) {
+        $('#input').autoComplete({
+            minChars: 2,
+            source: function(term, suggest) {
+                term = term.toLowerCase();
+                var choices = ['ActionScript', 'AppleScript', 'Asp'];
+                var matches = [];
+                for (i = 0; i < choices.length; i++)
+                    if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+                suggest(matches);
+            }
+        });
     }
 };
