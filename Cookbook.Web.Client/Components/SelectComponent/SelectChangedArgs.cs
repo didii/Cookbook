a39e6be23103944be53e9d7f1ae5b3ce7a5746b5
Dieter Van Broeck <VLM\OrdinaDVB>
2018-12-24 08:17:30 +0100

    Some random select component that doesn't work

---
+++ Cookbook.Web.Client/Components/SelectComponent/SelectChangedArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cookbook.Web.Client.Components.SelectComponent {
+    public class SelectChangedArgs<T> {
+        public SelectedChangedType Type { get; set; }
+        public int SelectedIndex { get; set; }
+        public T SelectedItem { get; set; }
+        public string Value { get; set; }
+    }
+
+    public enum SelectedChangedType {
+        FromCollection,
+        Other
+    }
+
+    public class SelectValue<T> {
+        public virtual string Value { get; set; }
+        public virtual string Display { get; set; }
+        public virtual T ObjectValue { get; set; }
+    }
+}
